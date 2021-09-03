835a135149295fc842173abd8df4f5ef6d7ad4f7
SAC05 <alcarazsebastian131@gmail.com>
2021-09-03 10:45:46 -0500

    -useless lines and spaces deleted -got the image locally -all hex values on Uppercase -useless methods deleted

--- src/app/components/header-nav/header-nav.component.ts
+++ src/app/components/header-nav/header-nav.component.ts
@@ -1,15 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-header-nav',
   templateUrl: './header-nav.component.html',
   styleUrls: ['./header-nav.component.css']
 })
-export class HeaderNavComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
-}
+export class HeaderNavComponent {}
